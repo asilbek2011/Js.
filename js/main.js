a6995a790f642377046d1985edb2b14c4333f4bc
asilbek2011 <asilbekabdurashidov00@gmail.com>
2023-11-10 10:08:13 -0500

    contact

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var pul = Number(prompt("Summani kiriting:"));
+var pul1 = 9433.34;
+
+var num = 500;
+var num1 = 250;
+var num2 = 120;
+
+console.log(num * pul1);
+console.log(num1 * pul1);
+console.log(num2 * 10354.03);
+console.log(4716670 + 2358335 + 1242483.6);
+
+var narx = 8317488.6;
+var heading = document.querySelector(".h1");
+
+if(8317488.6 <= pul){
+    heading.textContent = ("Oq yo'l, Alisher")
+}
+
+else {
+    heading.textContent = ("Alisher, ozgina sabr qilish kerak bo'lar ekan.")
+};
